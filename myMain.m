f386670833de1d12404f5341a3fa62260f5ec04c
amandasongmm <feijuejuanling@gmail.com>
2015-09-08 18:54:05 -0700

    calculating all single attribute matrix

--- myMain.m
+++ myMain.m
@@ -1,11 +1,14 @@
 
-clear; close all; clc; 
+clear; close all; clc;
 
 %% load data matrix
-fprintf('Loading facial attractiveness dataset.\n\n');
-load('attractData.mat');%Y and R
+load('./preprocessedData/dataNameList.mat');
+curItr = 2; % 2 stands for attractiveness.
+curDataName = dataNameList(curItr);
+fprintf(sprintf('Loading %s.\n\n',curDataName{:}));
+load(curDataName{:});%Y and R
 
-%% split test, cross-validation and training set. 
+%% split test, cross-validation and training set.
 trainRatio = 0.9; crossRatio = 0.01; testRatio = 0.09;
 [trainYnorm, trainYmean, trainSetR, crossInd, crossGT, testInd, testGT] = myPack.splitSet(Y, R, trainRatio, crossRatio, testRatio);
 
@@ -19,7 +22,7 @@ Theta = randn(num_users, num_features);
 init_params = [X(:);Theta(:)];
 
 %% Train the model
-params = myPack.fmincg (@(t)(myPack.cofiCostFunc(t, trainYnorm, trainSetR, num_users, num_faces, num_features, lambda)), init_params, options);            
+params = myPack.fmincg (@(t)(myPack.cofiCostFunc(t, trainYnorm, trainSetR, num_users, num_faces, num_features, lambda)), init_params, options);
 
 % Unfold the returned theta back into X and Theta
 X = reshape(params(1:num_faces*num_features), num_faces, num_features);
@@ -28,5 +31,5 @@ fprintf('Recommender system learning completed.\n');
 
 %% Test performance
 MSR = myPack.predictionError(X, Theta, R, trainYmean, testInd, testGT);
-fprintf('mean squared error = %4.2f.\n',MSR);
 
+fprintf('mean squared error = %4.2f.\n',MSR);
