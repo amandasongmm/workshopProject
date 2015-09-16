0905ecb0f43808c1683523186d9bea7f38006729
amandasongmm <feijuejuanling@gmail.com>
2015-09-15 20:21:57 -0700

    more editing

--- socialFeaturePCA.m
+++ socialFeaturePCA.m
@@ -1,6 +1,9 @@
 % socialFeaturePCA.m
 clear; clc; 
+
 load('./rawData/psy1FiVal.mat');
 load('./rawData/psy2FiVal.mat');
 socialFeatureMatrix = [psy1FiVal,psy2FiVal];
 
+socialFeatureMatrix = socialFeatureMatrix';% we treat each social feature as one variable, and 2222 as observation number
+% 2222*40 #observation * #variables = 
